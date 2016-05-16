f01758355ca022c86f073894a4625187913bfd0a
root-nishi <techno.nishi@gmail.com>
2016-05-16 22:18:02 +0530

    fix: correct index updates

--- Submission/SmartCab/smartcab/smartcab/agent.py
+++ Submission/SmartCab/smartcab/smartcab/agent.py
@@ -16,7 +16,8 @@ class LearningAgent(Agent):
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
         self.qt=QTable(0.5,0.5)
-        print "qt: ",self.qt
+        print '-'*80
+        print "\n\nqt: ",self.qt
     
 
     def reset(self, destination=None):
@@ -54,15 +55,16 @@ def run():
     e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=1)  # press Esc or close pygame window to quit
+    sim = Simulator(e, update_delay=0.00)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=100)  # press Esc or close pygame window to quit
+    print a.qt.printVal()
 
 
 class QTable(object):
 
     def __init__(self, alpha, gamma):
-        rnd=0*np.random.random_sample((2,4,4,4))
-        p4d = pd.Panel4D(rnd)
+        rnd0=0*np.random.random_sample((2,4,4,4))
+        p4d = pd.Panel4D(rnd0)
 
         Panel5D = panelnd.create_nd_panel_factory(klass_name   = 'Panel5D',
         orders  = [ 'action', 'labels','items','major_axis','minor_axis'],
@@ -71,15 +73,19 @@ class QTable(object):
         slicer  = pd.Panel4D,
         stat_axis    = 2)
 
-        p5d = Panel5D(dict(N = p4d,F=p4d,L=p4d,R=p4d))
+        p5d = Panel5D(dict(AN = p4d,BF=p4d,CL=p4d,DR=p4d))
         self.qtable=p5d
         self.alpha=alpha
         self.gamma=gamma
-        print "p5d:", p5d
+        #print "p5d:", p5d.values
+    
+    def printVal(self):
+        print "p5d",self.qtable.values
 
     def update(self, current_state, action, reward, next_state):
         current_state_df=pd.DataFrame(current_state.items())
-        if current_state_df.iloc[0,1] == 'green':
+        print "current_state:", current_state
+        if current_state_df.iloc[0,1] == 'red':
             light_code=0
         else:
             light_code=1
@@ -94,31 +100,31 @@ class QTable(object):
             oncoming_code=3
 
         if current_state_df.iloc[2,1] == None:
-            left_code=0
-        elif current_state_df.iloc[2,1] == 'forward':
-            left_code=1
-        elif current_state_df.iloc[2,1] == 'left':
-            left_code=2
-        elif current_state_df.iloc[2,1] == 'right':
-            left_code=3
-
-        if current_state_df.iloc[3,1] == None:
             right_code=0
-        elif current_state_df.iloc[3,1] == 'forward':
+        elif current_state_df.iloc[2,1] == 'forward':
             right_code=1
-        elif current_state_df.iloc[3,1] == 'left':
+        elif current_state_df.iloc[2,1] == 'left':
             right_code=2
-        elif current_state_df.iloc[3,1] == 'right':
+        elif current_state_df.iloc[2,1] == 'right':
             right_code=3
 
+        if current_state_df.iloc[3,1] == None:
+            left_code=0
+        elif current_state_df.iloc[3,1] == 'forward':
+            left_code=1
+        elif current_state_df.iloc[3,1] == 'left':
+            left_code=2
+        elif current_state_df.iloc[3,1] == 'right':
+            left_code=3
+
         if action == None:
-            action_code='N'
+            action_code='AN'
         elif action == 'forward':
-            action_code='F'
+            action_code='BF'
         elif action == 'left':
-            action_code='L'
+            action_code='CL'
         elif action == 'right':
-            action_code='R'
+            action_code='DR'
         
         print "action_code:",action_code,",light_code:",light_code,",oncoming_code:",oncoming_code,",left_code:",left_code,",right_code:",right_code,
 
@@ -129,7 +135,7 @@ class QTable(object):
         
         self.qtable.ix[action_code,light_code,oncoming_code,left_code,right_code]=new_val
 
-        print "q-Table:", self.qtable.values
+        #print "q-Table:", self.qtable.values
 
 if __name__ == '__main__':
     run()
