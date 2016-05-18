7e87fff25fb58856a2b8156bf204fd8f142879b3
root-nishi <techno.nishi@gmail.com>
2016-05-19 01:17:20 +0530

    feature: add navigation and deadline to state

--- Submission/SmartCab/smartcab/smartcab/agent.py
+++ Submission/SmartCab/smartcab/smartcab/agent.py
@@ -15,7 +15,7 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
-        self.qt=QTable(0.5,5)
+        self.qt=QTable(0.5,0.5)
         print '-'*80
         #print "\n\nqt: ",self.qt
     
@@ -23,7 +23,8 @@ class LearningAgent(Agent):
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-        self.qt.printVal()
+        totalTime=self.env.get_deadline(self)
+        self.qt.printVal(totalTime)
 
     def update(self, t):
         # Gather inputs
@@ -37,7 +38,7 @@ class LearningAgent(Agent):
         
         # TODO: Select action according to your policy
         #action = random.choice([None, 'forward', 'left', 'right'])
-        action = self.qt.get_next_action(current_state)
+        action = self.qt.get_next_action( self.next_waypoint, deadline, current_state)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
@@ -45,7 +46,7 @@ class LearningAgent(Agent):
         # TODO: Learn policy based on state, action, reward
         next_state=self.env.sense(self)
         #print current_state, action, reward, next_state
-        self.qt.update(current_state, action, reward, next_state)
+        self.qt.update(self.next_waypoint, deadline, current_state, action, reward)
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, current_state, action, reward)  # [debug]
 
 def run():
@@ -65,7 +66,7 @@ def run():
 class QTable(object):
 
     def __init__(self, alpha, gamma):
-        rnd0=0*np.random.random_sample((2,4,4,4))
+        rnd0=0*np.random.random_sample((4,100,512,2))
         p4d = pd.Panel4D(rnd0)
 
         Panel5D = panelnd.create_nd_panel_factory(klass_name   = 'Panel5D',
@@ -81,14 +82,16 @@ class QTable(object):
         self.gamma=gamma
         self.trial_count=0
         self.exploration_trials=60
-        self.reward=[0.5,1.0,1.0,1.0]
+        self.reward=np.zeros(4)
+        self.total_time=0
         #print "p5d:", p5d.values
     
-    def printVal(self):
+    def printVal(self, totalTime):
             self.trial_count += 1
-            print "p5d",self.qtable.values, ", \nself.trial_count:",self.trial_count
+            self.total_time=totalTime
+            print "\nself.trial_count:",self.trial_count
 
-    def update(self, current_state, action, reward, next_state):
+    def update(self, next_waypoint, deadline, current_state, action, reward):
         current_state_df=pd.DataFrame(current_state.items())
         #print "current_state:", current_state,"current_state_df:",current_state_df,",action:",action,",reward:"
         if current_state_df.iloc[0,1] == 'red':
@@ -99,20 +102,20 @@ class QTable(object):
         if current_state_df.iloc[1,1] == None:
             oncoming_code=0
         elif current_state_df.iloc[1,1] == 'forward':
-            oncoming_code=1
+            oncoming_code=32
         elif current_state_df.iloc[1,1] == 'right':
-            oncoming_code=2
+            oncoming_code=64
         elif current_state_df.iloc[1,1] == 'left':
-            oncoming_code=3
+            oncoming_code=96
 
         if current_state_df.iloc[2,1] == None:
             right_code=0
         elif current_state_df.iloc[2,1] == 'forward':
-            right_code=1
+            right_code=8
         elif current_state_df.iloc[2,1] == 'right':
-            right_code=2
+            right_code=16
         elif current_state_df.iloc[2,1] == 'left':
-            right_code=3
+            right_code=24
 
         if current_state_df.iloc[3,1] == None:
             left_code=0
@@ -122,6 +125,17 @@ class QTable(object):
             left_code=2
         elif current_state_df.iloc[3,1] == 'left':
             left_code=3
+        
+        traffic_code = oncoming_code + right_code + left_code
+
+        if next_waypoint == None:
+            nav_code=0
+        elif next_waypoint == 'forward':
+            nav_code=1
+        elif next_waypoint == 'right':
+            nav_code=2
+        elif next_waypoint == 'left':
+            nav_code=3
 
         if action == None:
             action_code='AN'
@@ -132,75 +146,84 @@ class QTable(object):
         elif action == 'left':
             action_code='DL'
         
-        #print "action_code:",action_code,",light_code:",light_code,",oncoming_code:",oncoming_code,",left_code:",left_code,",right_code:",right_code
+        print "action_code:",action_code,",nav_code:",nav_code,",deadline:",deadline,",traffic_code:",traffic_code,",light_code:",light_code,"time taken:-- ",(self.total_time-deadline)
 
-        prev_val = self.qtable.ix[action_code,light_code,oncoming_code,right_code,left_code]
+        prev_val = self.qtable.ix[action_code,nav_code, deadline, traffic_code,light_code]
         new_val=(1-self.alpha)*prev_val + self.alpha*reward
         
-        #print "prev_val: ",prev_val,", next_val:", new_val
+        print "prev_val: ",prev_val,", next_val:", new_val
         
-        self.qtable.ix[action_code,light_code,oncoming_code,right_code, left_code]=new_val
+        self.qtable.ix[action_code,nav_code, deadline, traffic_code,light_code]=new_val
 
         #print "q-Table:", self.qtable.values
 
-    def get_next_action(self, current_state):
-        #print "get_next_action:", current_state,", self.trial_count:",self.trial_count
+    def get_next_action(self, next_waypoint, deadline, current_state):
+        
         
         if self.trial_count < self.exploration_trials:
             return random.choice([None, 'forward', 'right', 'left'])
         else: # exploitation
             current_state_df=pd.DataFrame(current_state.items())
             print "getting next action for current_state:", current_state
-
-            if current_state_df.iloc[0,1] == 'red':
-                light_code=0
-            else:
-                light_code=1
-
-            if current_state_df.iloc[1,1] == None:
-                oncoming_code=0
-            elif current_state_df.iloc[1,1] == 'forward':
-                oncoming_code=1
-            elif current_state_df.iloc[1,1] == 'right':
-                oncoming_code=2
-            elif current_state_df.iloc[1,1] == 'left':
-                oncoming_code=3
+            
+        if current_state_df.iloc[0,1] == 'red':
+            light_code=0
+        else:
+            light_code=1
         
-            if current_state_df.iloc[2,1] == None:
-                right_code=0
-            elif current_state_df.iloc[2,1] == 'forward':
-                right_code=1
-            elif current_state_df.iloc[2,1] == 'right':
-                right_code=2
-            elif current_state_df.iloc[2,1] == 'left':
-                right_code=3
+        if current_state_df.iloc[1,1] == None:
+            oncoming_code=0
+        elif current_state_df.iloc[1,1] == 'forward':
+            oncoming_code=32
+        elif current_state_df.iloc[1,1] == 'right':
+            oncoming_code=64
+        elif current_state_df.iloc[1,1] == 'left':
+            oncoming_code=96
         
-            if current_state_df.iloc[3,1] == None:
-                left_code=0
-            elif current_state_df.iloc[3,1] == 'forward':
-                left_code=1
-            elif current_state_df.iloc[3,1] == 'right':
-                left_code=2
-            elif current_state_df.iloc[3,1] == 'left':
-                left_code=3
+        if current_state_df.iloc[2,1] == None:
+            right_code=0
+        elif current_state_df.iloc[2,1] == 'forward':
+            right_code=8
+        elif current_state_df.iloc[2,1] == 'right':
+            right_code=16
+        elif current_state_df.iloc[2,1] == 'left':
+            right_code=24
+        
+        if current_state_df.iloc[3,1] == None:
+            left_code=0
+        elif current_state_df.iloc[3,1] == 'forward':
+            left_code=1
+        elif current_state_df.iloc[3,1] == 'right':
+            left_code=2
+        elif current_state_df.iloc[3,1] == 'left':
+            left_code=3
+        
+        traffic_code = oncoming_code + right_code + left_code
+        
+        if next_waypoint == None:
+            nav_code=0
+        elif next_waypoint == 'forward':
+            nav_code=1
+        elif next_waypoint == 'right':
+            nav_code=2
+        elif next_waypoint == 'left':
+            nav_code=3
+                
+        
 
-            print "light_code:",light_code,",oncoming_code:",oncoming_code,",left_code:",left_code,",right_code:",right_code
-            print "None:", self.qtable.ix['AN'].values,"\n"
-            print "FWD:", self.qtable.ix['BF'].values,"\n"
-            print "RIGHT:", self.qtable.ix['CR'].values,"\n"
-            print "LEFT:", self.qtable.ix['DL'].values,"\n"
-            
-            self.reward[0]=self.qtable.ix['AN',light_code,oncoming_code,right_code,left_code]
-            self.reward[1]=self.qtable.ix['BF',light_code,oncoming_code,right_code,left_code]
-            self.reward[2]=self.qtable.ix['CR',light_code,oncoming_code,right_code,left_code]
-            self.reward[3]=self.qtable.ix['DL',light_code,oncoming_code,right_code,left_code]
-            max_rew=np.argmax(self.reward)
-           
-            suggested_action=[None, 'forward', 'right', 'left'][max_rew]
-            
-            print "\ntrial_count:",self.trial_count,"rewards: ", self.reward,",max:",max_rew,",suggested_action:",suggested_action
-            
-            return suggested_action
+        print "nav_code:",nav_code,",deadline:",deadline,",traffic_code:",traffic_code,",light_code:",light_code
+
+        self.reward[0]=self.qtable.ix['AN',nav_code, deadline, traffic_code,light_code]
+        self.reward[1]=self.qtable.ix['BF',nav_code, deadline, traffic_code,light_code]
+        self.reward[2]=self.qtable.ix['CR',nav_code, deadline, traffic_code,light_code]
+        self.reward[3]=self.qtable.ix['DL',nav_code, deadline, traffic_code,light_code]
+        max_rew=np.argmax(self.reward)
+       
+        suggested_action=[None, 'forward', 'right', 'left'][max_rew]
+        
+        print "\ntrial_count:",self.trial_count,"rewards: ", self.reward,",max:",max_rew,",suggested_action:",suggested_action
+        
+        return suggested_action
 
 
 if __name__ == '__main__':
