011c7c2848fb3ab01b8812ffb33254df50705e6c
root-nishi <techno.nishi@gmail.com>
2016-05-20 02:34:09 +0530

    refactor: hash func and deadline plot

---
+++ Submission/SmartCab/smartcab/smartcab/McHersheyHashMcHarshFace.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+
+class HashFunction(object):
+    """ Hashing Function to find index based on state"""
+
+    def hash4DState(self, next_waypoint, deadline, current_state):
+        """Find Index into Q-Table, for 4 Dimentions of state: Navigation, Deadline, Traffic, and Light. """
+        
+        current_state_df=pd.DataFrame(current_state.items())
+        
+        if current_state_df.iloc[0,1] == 'red':
+            light_code=0
+        else:
+            light_code=1
+        
+        if current_state_df.iloc[1,1] == None:
+            oncoming_code=0
+        elif current_state_df.iloc[1,1] == 'forward':
+            oncoming_code=32
+        elif current_state_df.iloc[1,1] == 'right':
+            oncoming_code=64
+        elif current_state_df.iloc[1,1] == 'left':
+            oncoming_code=96
+        
+        if current_state_df.iloc[2,1] == None:
+            right_code=0
+        elif current_state_df.iloc[2,1] == 'forward':
+            right_code=8
+        elif current_state_df.iloc[2,1] == 'right':
+            right_code=16
+        elif current_state_df.iloc[2,1] == 'left':
+            right_code=24
+        
+        if current_state_df.iloc[3,1] == None:
+            left_code=0
+        elif current_state_df.iloc[3,1] == 'forward':
+            left_code=1
+        elif current_state_df.iloc[3,1] == 'right':
+            left_code=2
+        elif current_state_df.iloc[3,1] == 'left':
+            left_code=3
+        
+        traffic_code = oncoming_code + right_code + left_code
+        
+        if next_waypoint == None:
+            nav_code=0
+        elif next_waypoint == 'forward':
+            nav_code=1
+        elif next_waypoint == 'right':
+            nav_code=2
+        elif next_waypoint == 'left':
+            nav_code=3
+
+        return nav_code, deadline, traffic_code,light_code
+
+
+    def hash5DState(self, action, next_waypoint, deadline, current_state):
+        """Find Index into Q-Table, for 4 Dimentions of state: Navigation, Deadline, Traffic, and Light. """
+
+        if action == None:
+            action_code='AN'
+        elif action == 'forward':
+            action_code='BF'
+        elif action == 'right':
+            action_code='CR'
+        elif action == 'left':
+            action_code='DL'
+
+        nav_code, deadline, traffic_code,light_code = self.hash4DState(next_waypoint, deadline, current_state)
+
+
+        return action_code, nav_code, deadline, traffic_code, light_code
--- Submission/SmartCab/smartcab/smartcab/agent.py
+++ Submission/SmartCab/smartcab/smartcab/agent.py
@@ -2,10 +2,12 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+from McHersheyHashMcHarshFace import HashFunction
 import pandas as pd
 from pandas.core import panelnd
 from pandas.core import panel4d
 import numpy as np
+import matplotlib.pyplot as plt
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -17,7 +19,6 @@ class LearningAgent(Agent):
         # TODO: Initialize any additional variables here
         self.qt=QTable(0.5,0.5)
         print '-'*80
-        #print "\n\nqt: ",self.qt
     
 
     def reset(self, destination=None):
@@ -30,10 +31,9 @@ class LearningAgent(Agent):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         current_state = self.env.sense(self)
-        #print "current_state:", current_state
+        
         deadline = self.env.get_deadline(self)
-        print "deadline: ",deadline
-        # TODO: Update state
+               # TODO: Update state
         
         
         # TODO: Select action according to your policy
@@ -45,9 +45,7 @@ class LearningAgent(Agent):
 
         # TODO: Learn policy based on state, action, reward
         next_state=self.env.sense(self)
-        #print current_state, action, reward, next_state
         self.qt.update(self.next_waypoint, deadline, current_state, action, reward)
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, current_state, action, reward)  # [debug]
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -60,7 +58,6 @@ def run():
     # Now simulate it
     sim = Simulator(e, update_delay=0.0)  # reduce update_delay to speed up simulation
     sim.run(n_trials=100)  # press Esc or close pygame window to quit
-    #print a.qt.printVal()
 
 
 class QTable(object):
@@ -81,147 +78,56 @@ class QTable(object):
         self.alpha=alpha
         self.gamma=gamma
         self.trial_count=0
-        self.exploration_trials=60
+        self.exploration_trials=40
         self.reward=np.zeros(4)
         self.total_time=0
-        #print "p5d:", p5d.values
+        self.trial_steps_df=pd.DataFrame(np.random.randint(1, size=(100,2)))
+        pd.set_option("display.max_rows",100)
+
+    
     
     def printVal(self, totalTime):
             self.trial_count += 1
             self.total_time=totalTime
-            print "\nself.trial_count:",self.trial_count
+    
 
     def update(self, next_waypoint, deadline, current_state, action, reward):
-        current_state_df=pd.DataFrame(current_state.items())
-        #print "current_state:", current_state,"current_state_df:",current_state_df,",action:",action,",reward:"
-        if current_state_df.iloc[0,1] == 'red':
-            light_code=0
-        else:
-            light_code=1
-
-        if current_state_df.iloc[1,1] == None:
-            oncoming_code=0
-        elif current_state_df.iloc[1,1] == 'forward':
-            oncoming_code=32
-        elif current_state_df.iloc[1,1] == 'right':
-            oncoming_code=64
-        elif current_state_df.iloc[1,1] == 'left':
-            oncoming_code=96
-
-        if current_state_df.iloc[2,1] == None:
-            right_code=0
-        elif current_state_df.iloc[2,1] == 'forward':
-            right_code=8
-        elif current_state_df.iloc[2,1] == 'right':
-            right_code=16
-        elif current_state_df.iloc[2,1] == 'left':
-            right_code=24
-
-        if current_state_df.iloc[3,1] == None:
-            left_code=0
-        elif current_state_df.iloc[3,1] == 'forward':
-            left_code=1
-        elif current_state_df.iloc[3,1] == 'right':
-            left_code=2
-        elif current_state_df.iloc[3,1] == 'left':
-            left_code=3
+       
+        self.trial_steps_df.set_value(self.trial_count,1,deadline)
+        hashF = HashFunction()
+        action_code,nav_code, deadline, traffic_code,light_code = hashF.hash5DState(action, next_waypoint, deadline, current_state)
         
-        traffic_code = oncoming_code + right_code + left_code
-
-        if next_waypoint == None:
-            nav_code=0
-        elif next_waypoint == 'forward':
-            nav_code=1
-        elif next_waypoint == 'right':
-            nav_code=2
-        elif next_waypoint == 'left':
-            nav_code=3
-
-        if action == None:
-            action_code='AN'
-        elif action == 'forward':
-            action_code='BF'
-        elif action == 'right':
-            action_code='CR'
-        elif action == 'left':
-            action_code='DL'
-        
-        print "action_code:",action_code,",nav_code:",nav_code,",deadline:",deadline,",traffic_code:",traffic_code,",light_code:",light_code,"time taken:-- ",(self.total_time-deadline)
 
         prev_val = self.qtable.ix[action_code,nav_code, deadline, traffic_code,light_code]
         new_val=(1-self.alpha)*prev_val + self.alpha*reward
         
-        print "prev_val: ",prev_val,", next_val:", new_val
-        
         self.qtable.ix[action_code,nav_code, deadline, traffic_code,light_code]=new_val
 
-        #print "q-Table:", self.qtable.values
-
+        if(self.trial_count==99 and deadline==20):
+            print " STEPS: : ",  self.trial_steps_df
+            self.trial_steps_df.plot()
+            plt.show()
+    
+    
     def get_next_action(self, next_waypoint, deadline, current_state):
         
-        
-        if self.trial_count < self.exploration_trials:
+        #print "trial: ", self.trial_count,"  deadline: ", deadline, " of ", self.total_time
+        if self.trial_count < self.exploration_trials: # explotation
             return random.choice([None, 'forward', 'right', 'left'])
         else: # exploitation
-            current_state_df=pd.DataFrame(current_state.items())
-            print "getting next action for current_state:", current_state
-            
-        if current_state_df.iloc[0,1] == 'red':
-            light_code=0
-        else:
-            light_code=1
-        
-        if current_state_df.iloc[1,1] == None:
-            oncoming_code=0
-        elif current_state_df.iloc[1,1] == 'forward':
-            oncoming_code=32
-        elif current_state_df.iloc[1,1] == 'right':
-            oncoming_code=64
-        elif current_state_df.iloc[1,1] == 'left':
-            oncoming_code=96
-        
-        if current_state_df.iloc[2,1] == None:
-            right_code=0
-        elif current_state_df.iloc[2,1] == 'forward':
-            right_code=8
-        elif current_state_df.iloc[2,1] == 'right':
-            right_code=16
-        elif current_state_df.iloc[2,1] == 'left':
-            right_code=24
-        
-        if current_state_df.iloc[3,1] == None:
-            left_code=0
-        elif current_state_df.iloc[3,1] == 'forward':
-            left_code=1
-        elif current_state_df.iloc[3,1] == 'right':
-            left_code=2
-        elif current_state_df.iloc[3,1] == 'left':
-            left_code=3
-        
-        traffic_code = oncoming_code + right_code + left_code
-        
-        if next_waypoint == None:
-            nav_code=0
-        elif next_waypoint == 'forward':
-            nav_code=1
-        elif next_waypoint == 'right':
-            nav_code=2
-        elif next_waypoint == 'left':
-            nav_code=3
-                
-        
+            hashF = HashFunction()
+            nav_code, deadline, traffic_code,light_code=hashF.hash4DState(next_waypoint, deadline, current_state)
+           
 
-        print "nav_code:",nav_code,",deadline:",deadline,",traffic_code:",traffic_code,",light_code:",light_code
-
-        self.reward[0]=self.qtable.ix['AN',nav_code, deadline, traffic_code,light_code]
-        self.reward[1]=self.qtable.ix['BF',nav_code, deadline, traffic_code,light_code]
-        self.reward[2]=self.qtable.ix['CR',nav_code, deadline, traffic_code,light_code]
-        self.reward[3]=self.qtable.ix['DL',nav_code, deadline, traffic_code,light_code]
-        max_rew=np.argmax(self.reward)
+            self.reward[0]=self.qtable.ix['AN',nav_code, deadline, traffic_code,light_code]
+            self.reward[1]=self.qtable.ix['BF',nav_code, deadline, traffic_code,light_code]
+            self.reward[2]=self.qtable.ix['CR',nav_code, deadline, traffic_code,light_code]
+            self.reward[3]=self.qtable.ix['DL',nav_code, deadline, traffic_code,light_code]
+            max_rew=np.argmax(self.reward)
        
-        suggested_action=[None, 'forward', 'right', 'left'][max_rew]
+            suggested_action=[None, 'forward', 'right', 'left'][max_rew]
         
-        print "\ntrial_count:",self.trial_count,"rewards: ", self.reward,",max:",max_rew,",suggested_action:",suggested_action
+        
         
         return suggested_action
 
